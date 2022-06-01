6d2138dad80cdebc4c9984e6ee91071f2ae46da7
Saurabh Chugh <saurabh.chugh@telekom-digital.com>
2022-06-02 00:48:22 +0530

    Added routing, routes, Input/Label/UserSettings/NotFound component with basic prettierrc.js setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import { useRoutes } from 'react-router-dom';
+import UserSettings from './pages/UserSettings';
+import NotFound from './pages/NotFound';
+
 function App() {
-  return (
-    <div>
-      App component
-    </div>
-  );
+  let element = useRoutes([
+    { path: '/', element: <UserSettings /> },
+    { path: 'user-settings', element: <UserSettings /> },
+    { path: '*', element: <NotFound /> },
+  ]);
+
+  return element;
 }
 
 export default App;
---
+++ src/pages/NotFound/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <>The page you are looking for is not available, please check the Url</>
+    );
+}
+
+export default NotFound;
